751f88343284ad0ca173ef9e89f132b0946d8a1e
Abdelrahmanmoussan <abdelrahmanmoussan339@gmail.com>
2025-04-30 04:22:16 +0300

    Add three Repos and IRepos

---
+++ EducationCenter.DataAccess/Repository/StudentRepository.cs
@@ -0,0 +1,22 @@
+﻿using EducationCenter.DataAccess.Data;
+using EducationCenter.DataAccess.Repository.IRepository;
+using EducationCenter.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenter.DataAccess.Repository
+{
+    internal class StudentRepository : Repository<Student>, IStudentRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public StudentRepository(AppDbContext appDbContext) : base(appDbContext) 
+        {
+            dbContext = appDbContext;
+        }
+    }
+}
---
+++ EducationCenter.DataAccess/Repository/TeacherRepository.cs
@@ -0,0 +1,21 @@
+﻿using EducationCenter.DataAccess.Data;
+using EducationCenter.DataAccess.Repository.IRepository;
+using EducationCenter.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenter.DataAccess.Repository
+{
+    internal class TeacherRepository : Repository<Teacher>, ITeacherRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public TeacherRepository(AppDbContext appDbContext) : base(appDbContext) 
+        {
+            dbContext = appDbContext;
+        }
+    }
+}
