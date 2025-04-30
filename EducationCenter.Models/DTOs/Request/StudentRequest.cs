46bf8119bce4560d01255f3263bed6a9d331be86
Mostafa Anis ELnaggar <149039636+mostafa20004@users.noreply.github.com>
2025-04-30 19:43:05 +0300

    Add Dto For Identity

---
+++ EducationCenter.Models/DTOs/Request/StudentRequest.cs
@@ -0,0 +1,34 @@
+﻿using EducationCenter.Models.Enums;
+using EducationCenter.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenter.Models.DTOs.Request
+{
+    internal class StudentRequest
+    {
+        [Required]
+        public int AcademicYearID { get; set; }
+        public AcademicYear AcademicYear { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string ParentName { get; set; }
+
+        [Required]
+        [Phone]
+        public string ParentPhone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string ParentMail { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public ParentStatus ParentStatus { get; set; }
+    }
+}
---
+++ EducationCenter.Models/DTOs/Request/TeacherRequest.cs
@@ -0,0 +1,21 @@
+﻿using EducationCenter.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCenter.Models.DTOs.Request
+{
+    internal class TeacherRequest
+    {
+
+        [Required]
+        public int SubjectID { get; set; }
+
+        [Required]
+        public int AcademicYearID { get; set; }
+        
+    }
+}
