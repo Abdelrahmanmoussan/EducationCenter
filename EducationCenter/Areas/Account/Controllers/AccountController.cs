3a8d879d0f3f82eab3ebd5d22a76706630c40e27
Abdelrahmanmoussan <abdelrahmanmoussan339@gmail.com>
2025-05-01 14:39:05 +0300

    add AccountController

---
+++ EducationCenter/Areas/Account/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationCenter.Areas.Account.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
